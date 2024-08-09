3f801eb3b9a8a58a9a7bf621d81003f3b17e8e04
CarolineScla <124374875+CarolineScla@users.noreply.github.com>
2024-08-09 13:34:19 +0000

    timeout :'(

--- get_next_line.h
+++ get_next_line.h
@@ -14,16 +14,17 @@
 
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
 # include <stdlib.h>
-# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
 
 char	*get_next_line(int fd);
+char	*extract_line(char **leftover);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(const char *s);
-size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*extract_line(char **leftover);
+char	*ft_strcpy(char *dest, const char *src);
+size_t	ft_strlen(const char *s);
 
 #endif
