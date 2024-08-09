aa2759adc7d606b27056fdaa7a3c6e528b44d7b7
CarolineScla <124374875+CarolineScla@users.noreply.github.com>
2024-08-09 16:09:06 +0000

    ko

--- get_next_line.c
+++ get_next_line.c
@@ -6,55 +6,89 @@
 /*   By: csclavon <csclavon@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 13:45:10 by csclavon          #+#    #+#             */
-/*   Updated: 2024/08/06 14:27:28 by csclavon         ###   ########.fr       */
+/*   Updated: 2024/08/09 18:08:25 by csclavon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "get_next_line.h"
 
 char	*extract_line(char **leftover);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 char	*get_next_line(int fd)
 {
-	static char *leftover = NULL;
-	char buffer[1];
-	ssize_t bytes_read;
-	char *newline_pos;
+	static char	*leftover = NULL;
+	char		buffer[1];
+	ssize_t		bytes_read;
+	char		*newline_pos;
+	char		*new_leftover;
+	char		*line;
 
 	if (fd < 0)
 		return (NULL);
-
-	while ((bytes_read = read(fd, buffer, 1)) > 0) {
+	while ((bytes_read = read(fd, buffer, 1)) > 0)
+	{
 		buffer[bytes_read] = '\0';
-		char *temp = ft_strjoin(leftover ? leftover : "", buffer);
-		free(leftover);
-		leftover = temp;
+		if (leftover != NULL)
+		{
+			new_leftover = ft_strjoin(leftover, buffer);
+			free(leftover);
+			leftover = new_leftover;
+		}
+		else
+			leftover = ft_strjoin("", buffer);
 		newline_pos = ft_strchr(leftover, '\n');
 		if (newline_pos)
-			break;
+			break ;
 	}
-
 	if (bytes_read < 0 || (bytes_read == 0 && (!leftover || !*leftover)))
 		return (NULL);
-
-	char *line = extract_line(&leftover);
+	*line = extract_line(&leftover);
 	return (line);
 }
 
 char	*extract_line(char **leftover)
 {
-	char *line;
-	char *newline_pos;
+	char	*line;
+	char	*newline_pos;
+	char	*new_leftover;
 
 	newline_pos = ft_strchr(*leftover, '\n');
-	if (newline_pos) {
+	if (newline_pos)
+	{
 		line = ft_substr(*leftover, 0, newline_pos - *leftover + 1);
-		char *new_leftover = ft_strdup(newline_pos + 1);
+		new_leftover = ft_strdup(newline_pos + 1);
 		free(*leftover);
 		*leftover = new_leftover;
-	} else {
+	}
+	else
+	{
 		line = ft_strdup(*leftover);
 		free(*leftover);
 		*leftover = NULL;
 	}
 	return (line);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+	int		k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	str = (char *) malloc((ft_strlen(s1) + ft_strlen(s2)) + 1);
+	if (str == NULL)
+		return (NULL);
+	while (s1[j] != '\0')
+		str[i++] = s1[j++];
+	while (s2[k] != '\0')
+		str[i++] = s2[k++];
+	str[i] = '\0';
+	return (str);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: csclavon <csclavon@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 13:45:10 by csclavon          #+#    #+#             */
-/*   Updated: 2024/08/06 16:35:21 by csclavon         ###   ########.fr       */
+/*   Updated: 2024/08/09 17:58:11 by csclavon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,9 +47,7 @@ size_t	ft_strlen(const char *s)
 
 	i = 0;
 	while (s[i] != '\0')
-	{
 		i++;
-	}
 	return (i);
 }
 
@@ -75,29 +73,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		start++;
 		len--;
 	}
-	str[i] = '\0';
-	return (str);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*str;
-	int		i;
-	int		j;
-	int		k;
-
-	i = 0;
-	j = 0;
-	k = 0;
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-	str = (char *) malloc((ft_strlen(s1) + ft_strlen(s2)) + 1);
-	if (str == NULL)
-		return (NULL);
-	while (s1[j] != '\0')
-		str[i++] = s1[j++];
-	while (s2[k] != '\0')
-		str[i++] = s2[k++];
 	str[i] = '\0';
 	return (str);
 }
