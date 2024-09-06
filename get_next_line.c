8501e16ffe813072f0587f969af335853e217b76
CarolineScla <124374875+CarolineScla@users.noreply.github.com>
2024-09-06 12:02:04 +0000

    finish

--- get_next_line.c
+++ get_next_line.c
@@ -6,10 +6,9 @@
 /*   By: csclavon <csclavon@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 13:45:10 by csclavon          #+#    #+#             */
-/*   Updated: 2024/08/09 18:08:25 by csclavon         ###   ########.fr       */
+/*   Updated: 2024/08/06 14:27:28 by csclavon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "get_next_line.h"
 
 char	*extract_line(char **leftover);
@@ -17,52 +16,40 @@ char	*ft_strjoin(char const *s1, char const *s2);
 
 char	*get_next_line(int fd)
 {
-	static char	*leftover = NULL;
-	char		buffer[1];
-	ssize_t		bytes_read;
-	char		*newline_pos;
-	char		*new_leftover;
-	char		*line;
-
+	static char *leftover = NULL;
+	char buffer[1];
+	ssize_t bytes_read;
+	char *newline_pos;
 	if (fd < 0)
 		return (NULL);
-	while ((bytes_read = read(fd, buffer, 1)) > 0)
-	{
+	while ((bytes_read = read(fd, buffer, 1)) > 0) {
 		buffer[bytes_read] = '\0';
-		if (leftover != NULL)
-		{
-			new_leftover = ft_strjoin(leftover, buffer);
-			free(leftover);
-			leftover = new_leftover;
-		}
-		else
-			leftover = ft_strjoin("", buffer);
+		char *temp = ft_strjoin(leftover ? leftover : "", buffer);
+		free(leftover);
+		leftover = temp;
 		newline_pos = ft_strchr(leftover, '\n');
 		if (newline_pos)
-			break ;
+			break;
 	}
 	if (bytes_read < 0 || (bytes_read == 0 && (!leftover || !*leftover)))
 		return (NULL);
-	*line = extract_line(&leftover);
+
+	char *line = extract_line(&leftover);
 	return (line);
 }
 
 char	*extract_line(char **leftover)
 {
-	char	*line;
-	char	*newline_pos;
-	char	*new_leftover;
+	char *line;
+	char *newline_pos;
 
 	newline_pos = ft_strchr(*leftover, '\n');
-	if (newline_pos)
-	{
+	if (newline_pos) {
 		line = ft_substr(*leftover, 0, newline_pos - *leftover + 1);
-		new_leftover = ft_strdup(newline_pos + 1);
+		char *new_leftover = ft_strdup(newline_pos + 1);
 		free(*leftover);
 		*leftover = new_leftover;
-	}
-	else
-	{
+	} else {
 		line = ft_strdup(*leftover);
 		free(*leftover);
 		*leftover = NULL;
